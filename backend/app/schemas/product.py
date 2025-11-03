eb3aa3bd6cf0fba915adf20b10c71b52abd35ae7
SuperHippoCoder <golisevpavel122@gmail.com>
2025-11-03 20:30:51 +0700

    Привет

--- backend/app/schemas/product.py
+++ backend/app/schemas/product.py
@@ -5,6 +5,28 @@ from .category import CategoryResponse
 
 
 class ProductBase(BaseModel):
-    name: str = Field(..., min_length=5, max_length=200,
-                            description="Product name")
+    name: str = Field(..., min_length=5, max_length=200, description="Product name")
     description: Optional[str] = Field(None, description="Product description")
+    price: float = Field(..., gt=0, description="Product price (Must be greater than 0)")
+    category_id: int = Field(..., description="Category ID")
+    image_url: Optional[str] = Field(None, description="Product image URL")
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductResponse(ProductBase):
+    id: int = Field(..., description="Unique product ID")
+    name: str
+    description: Optional[str]
+    price: float
+    category_id: int
+    image_url: Optional[str]
+    created_at: datetime
+    category: CategoryResponse = Field(..., description="Product category details")
+
+    class Config:
+        form_attributes = True
+
+class ProductListResponse(BaseModel):
+    products: list[ProductResponse]
+    total: int = Field(..., description="Total number of products")
